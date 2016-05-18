5ccc42d7fbd323f6186daeb52a37b5f15452d0b9
putriab <putriabsmkn1@gmail.com>
2016-05-18 12:58:38 +0700

    2

---
+++ login.js
@@ -0,0 +1,12 @@
+angular
+  .module('app', ['ngMaterial'])
+  .controller('DemoCtrl', function($scope) {
+    $scope.user = {
+      name: 'John Doe',
+      email: '',
+      phone: '',
+      address: 'Mountain View, CA',
+      donation: 19.99
+    };
+  });
+
